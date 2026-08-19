d06cad890fe9a9ee39a1452c0af3a48c940bbf76
BylelLongelinBis <bylel.lgln@gmail.com>
2026-08-19 13:46:10 +0200

    Add Playwright tests for accessibility switch functionality

---
+++ tests/accessible-mode.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('/');
+});
+
+
+test('Accessibility switch is not checked by default', async ({ page }) => {
+  await expect(page.getByRole('checkbox', { name: /Activer le mode accessible/ })).not.toBeChecked();
+});
+
+test('Accessibility switch is working', async ({ page }) => {
+    await page.locator('.a11y-toggle').check()
+    await expect(page.getByRole('checkbox', { name: /Activer le mode accessible/ })).toBeChecked();
+});
